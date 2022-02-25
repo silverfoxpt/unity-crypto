7c32221649c5b1745c1d78a83b321fd0b3139116
Silverfox <77423595+silverfoxpt@users.noreply.github.com>
2022-02-25 22:35:08 +0700

    Revert "Create letter columns, test build 1st"
    This reverts commit 54d01d3be3ca2964ee2fe3eefc77923e0189dcd9.

--- CryptoG/Assets/Scripts/EnigmaInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnigmaInfo : MonoBehaviour
-{
-    public static int defaultLength = 26;
-    public static string defaultAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-}
