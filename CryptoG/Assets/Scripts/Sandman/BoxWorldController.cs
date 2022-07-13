b4338537062e69bae111ef9b9c1f2ceab63b5241
Silverfox <77423595+silverfoxpt@users.noreply.github.com>
2022-07-13 22:28:10 +0700

    Vacation save

--- CryptoG/Assets/Scripts/Sandman/BoxWorldController.cs
+++ CryptoG/Assets/Scripts/Sandman/BoxWorldController.cs
@@ -16,7 +16,7 @@ public class BoxWorldController : MonoBehaviour
     struct blockInfo
     {
         public int id;
-        public float lifeTime;
+        public int lifeTime;
         public float velocity;
         public Color col;
         public int updated;
@@ -34,13 +34,59 @@ public class BoxWorldController : MonoBehaviour
     [Space(10)]
     [Header("References")]
     [SerializeField] private ComputeShader applyTexComp;
+    [SerializeField] private ComputeShader updateComp;
+
+    [Space(10)]
+    [Header("Others")]
+    [SerializeField] private bool slowUp = true;
+    [SerializeField] private float delay = 0.1f;
+
+    [Space(10)]
+    [SerializeField] private bool debug = false;
+    [SerializeField] private Vector2Int debugPoint;
 
     private blockInfo[] blocks, writeBlocks;
 
+    #region startMethods
     void Start()
     {
         InitializePlayField();    
         InitializeDataArray();
+
+        //debug - place some test sand
+        //PlaceTestBlocks();
+
+        if (slowUp)
+        {
+            StartCoroutine(SlowUpdate());
+        }
+    }
+
+    private void PlaceTestBlocks()
+    {
+        for (int i = 100 - penSize/2; i <= 100 + penSize/2; i++)
+        {
+            for (int j = 100 - penSize/2; j <= 100 + penSize/2; j++)
+            {
+                int toArr = ToOneDi(i, j); //Debug.Log(toArr);
+                if (toArr < 0 || toArr >= size.x * size.y) {continue;} //skip
+
+                if (blocks[toArr].id == -1) //air
+                {
+                    board.board.SetPixelDirect(new Vector2Int(i, j), Color.yellow, false);
+
+                    //need revision
+                    blockInfo newBlock = new blockInfo(); 
+                    newBlock.id = currentDrawID;
+                    newBlock.lifeTime = int.MaxValue;
+                    newBlock.col = Color.yellow;
+                    newBlock.updated = 0;
+
+                    blocks[toArr] = newBlock;
+                }
+            }
+        }
+        board.board.ApplyColor();
     }
 
     private void InitializeDataArray()
@@ -73,20 +119,52 @@ public class BoxWorldController : MonoBehaviour
         board.pen.allowedDraw = false;
         //board.board.SetPixelDirect(new Vector2Int(100, 100), Color.black, true);
     }
+    #endregion
+
+    #region updateMethods
+    IEnumerator SlowUpdate()
+    {
+        while(true)
+        {
+            CheckColoring();
+            UpdateAllBlocks();
+
+            //update
+            WriteBlockToTex(blocks, board.pen.rend);
+            board.board.SetTexture(board.pen.rend); 
+
+            yield return new WaitForSeconds(delay);
+        }
+    }
 
     private void Update()
     {
-        CheckColoring();
-        UpdateAllBlocks();
+        if (!slowUp)
+        {
+            CheckColoring();
+            UpdateAllBlocks();
 
-        //update
-        WriteBlockToTex(blocks, board.pen.rend);
-        board.board.SetTexture(board.pen.rend); 
+            //update
+            WriteBlockToTex(blocks, board.pen.rend);
+            board.board.SetTexture(board.pen.rend); 
+
+            //double buff boy
+            //Array.Copy(writeBlocks, blocks, size.x * size.y);
+        }
+
+        if (debug)
+        {
+            string de = ""; int idx = ToOneDi(debugPoint.x, debugPoint.y);
+            de += blocks[idx].id.ToString() + "\n";
+            de += blocks[idx].col.ToString();
+
+            Debug.Log(de);
+        }
     }
 
     private void WriteBlockToTex(blockInfo[] bo, RenderTexture currend)
     {
-        int wholeSize = sizeof(int) + sizeof(float) * 6 + sizeof(int); //Debug.Log(wholeSize);        
+        int wholeSize = sizeof(int) * 3 + sizeof(float) * 5; 
         ComputeBuffer applyBuffer = new ComputeBuffer(size.x * size.y, wholeSize);
         applyBuffer.SetData(bo);
 
@@ -99,12 +177,35 @@ public class BoxWorldController : MonoBehaviour
         applyTexComp.SetTexture(kernelIdx, "rend", currend);
         applyTexComp.Dispatch(kernelIdx, size.x / 16, size.y / 16, 1); //buggy?
 
+        applyBuffer.GetData(bo); 
+
         applyBuffer.Dispose();
     }
 
     private void UpdateAllBlocks()
     {
-        
+        writeBlocks = new blockInfo[size.x * size.y];
+
+        int wholeSize = sizeof(int) * 3 + sizeof(float) * 5;//Debug.Log(wholeSize);        
+        ComputeBuffer updateBuffer = new ComputeBuffer(size.x * size.y, wholeSize);
+        updateBuffer.SetData(blocks);
+
+        ComputeBuffer updateWriteBuffer = new ComputeBuffer(size.x * size.y, wholeSize);
+        updateWriteBuffer.SetData(writeBlocks);
+
+        int kernelIdx = updateComp.FindKernel("UpTex");
+        updateComp.SetBuffer(kernelIdx, "readBlock", updateBuffer);
+        updateComp.SetBuffer(kernelIdx, "writeBlock", updateWriteBuffer);
+
+        updateComp.SetInt("sizeX", size.x);
+        updateComp.SetInt("sizeY", size.y);
+
+        updateComp.Dispatch(kernelIdx, size.x / 8, size.y / 8, 1); //buggy?
+
+        updateWriteBuffer.GetData(blocks);
+
+        updateBuffer.Dispose();
+        updateWriteBuffer.Dispose();
     }
 
     private void CheckColoring()
@@ -151,4 +252,5 @@ public class BoxWorldController : MonoBehaviour
         int newX = size.y - 1 - j, newY = i;
         return (size.x * newX + (newY));
     }
+    #endregion
 }
